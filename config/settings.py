23e3c98eaf49f624268ba7f38d25363964bbde58
Bekjan728 <abdykadyrovbekjan077@gmail.com>
2024-05-24 11:00:02 +0600

    check

--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,6 @@ INSTALLED_APPS = [
 
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-
-    'apps.account',
-    'apps.academics',
-    'apps.admissions',
-    'apps.news',
 ]
 
 MIDDLEWARE = [
